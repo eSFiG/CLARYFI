5e7e63a4090d4f7d0b828099371adc942e07222e
eSFiG <krutxxxman2000@gmail.com>
2026-02-20 07:46:05 +0000

    Fix issue

---
+++ tests/WebTestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseWebTestCase;
+
+abstract class WebTestCase extends BaseWebTestCase
+{
+}
